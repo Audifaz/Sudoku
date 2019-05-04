967d8aa0694f49931bde58869dc0c485aa69eda7
Audifaz <emilio.aguila@hotmail.com>
2019-05-04 07:57:07 -0500

    Update Sudoku.java

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -25,26 +25,22 @@ public class Sudoku extends JFrame implements ActionListener{
     public final int frameHeight = 640;
     public final int frameXOrigin = 425;
     public final int frameYOrigin = 100;
-    public final int buttonW =45;
-    public final int buttonH =45;    
-    JMenu menu;
-    public final JButton seven;    
+    public final int buttonW =60;
+    public final int buttonH =59;    
+    JMenu menu;    
     
     
     public static void main(String[] args) {
            Sudoku frame= new Sudoku();
-           frame.setVisible(true); 
-        try {
-            frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("sudokuGrid.png")))));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }           
+           frame.setVisible(true);          
     }
     
     
     public Sudoku(){
 
-        
+        /*https://docs.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+        Oracle (2017) "How to Set the Look and Feel". Accessed the 4 of May of 2019
+        */
         try {
             UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
         } catch (Exception e) {
@@ -103,13 +99,40 @@ public class Sudoku extends JFrame implements ActionListener{
         setJMenuBar(jmenubar);
         jmenubar.add(menu); 
         /* End Menus */
-        /*Buttons*/
-        //seven
-        seven = new JButton("7");
-        seven.setBounds(30,45,buttonW,buttonH);
-        seven.setBackground(new java.awt.Color(245, 245, 228));
-        seven.setOpaque(true);       
-        contentPane.add(seven);
+        int i, j, k, l,a,b;
+        JButton[][] MatrizBotones = new JButton[9][9];
+        a=25;
+        b=24;
+        //Inspirado en el código de https://prografacilsite.wordpress.com/2017/06/15/como-crear-una-matriz-grafica-de-botones-en-java-dinamicamente/
+        //Devjtello (2017) "Como hacer una matriz gráfica de botones en java". Accesado el 2 de Mayo de 2019
+        for (i = 0; i < 9; i++)
+                {
+                    for (j = 0; j < 9; j++)
+                    {
+                        JButton btnNuevo = new JButton();
+                        btnNuevo.setText(Integer.toString(i+1) + Integer.toString(j+1));
+                        btnNuevo.setBackground(new java.awt.Color(238, 238, 238));
+                        
+                        Font font = new Font("Arial", Font.BOLD, 19);
+                        btnNuevo.setFont(font);                        
+                        MatrizBotones[i][j] = btnNuevo;
+                        contentPane.add(MatrizBotones[i][j]);
+                        MatrizBotones[i][j].setBounds(a,b,buttonW,buttonH);
+                        MatrizBotones[i][j].addActionListener(this);
+                        
+                        if(a % 3 ==0){
+                            a=a+1;
+                        }
+                        a=a+61;
+                        System.out.println("["+a+"]"+"["+b+"]");
+                    }
+                    a=25;
+                        if(b % 3==0){
+                            b=b+1;
+                        }                   
+                    b=b+61;
+                    j=1;
+                }
         /*End Buttons*/
         setDefaultCloseOperation( EXIT_ON_CLOSE ); 
     }
@@ -117,8 +140,17 @@ public class Sudoku extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e){
         if(e.getSource() instanceof JButton){
-        System.out.println("You got it");
+        JButton clickedButton = (JButton)e.getSource();
+        String buttonText = clickedButton.getText();    
+        setTitle(buttonText);
         }
+    else{  
+        JMenuItem clickedMenu = (JMenuItem)e.getSource();
+        String menuText = clickedMenu.getText();  
+        if( ((menuText.equals("Exit")))){
+            System.exit(0);
+        }
+    }         
         
     }
 }
