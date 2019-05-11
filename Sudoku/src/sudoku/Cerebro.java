51c76b802f4d0e659e5c38dee97a6872749c11b9
MauricioRTG <44274231+MauricioRTG@users.noreply.github.com>
2019-05-11 15:54:51 -0500

    Cerebro con todas las funciones

--- Sudoku/src/sudoku/Cerebro.java
+++ Sudoku/src/sudoku/Cerebro.java
@@ -5,39 +5,37 @@
  */
 
 package sudoku;
-
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
-import static java.lang.Math.random;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.file.Files;
-
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import javax.swing.*;
 /**
  *
- * @authors Emilio Águila Escalante - 163009
- *         Jorge Tenorio - 
- *         Mauricio Tenorio -
+ * @author Emilio Águila Escalante - 163009
+ *         Jorge Tenorio Guzmán - 162107
+ *         Mauricio Tenorio Guzmán- 162106
  */
-
-
 public class Cerebro {
-    public int[][] matrixI, matrixP, matrixS;
-    public String[][] matrizPP;
-    public File archivo;
-    public Scanner lector;
-    
-    public Cerebro(){
-        matrixI=new int[9][9];
-        matrixS=new int[9][9];
-        matrixP=new int[9][9];     
-	matrizPP= new String[9][9];
+   public int [][] matrixI;
+   public String [][] matrixP;
+   public int [][] matrixS;
+  
+   //public  Sudoku sud;
+   
+    public String contenido;
+    public Cerebro() {
+        
+         matrixI= new int[9][9];
+         matrixP= new String[9][9];
+         matrixS= new int[9][9];
+         // sud = new Sudoku();
+         
     }
-    
     public void startG(){
-           File f = new File("C:\\Users\\LIVERPOOL I5\\Documents\\NetBeansProjects\\Sudoku\\src\\sudoku\\Matrizinicial.txt");
+         File f = new File("Matrizinicial.txt");
 
 		Scanner s;
 		try {	
@@ -50,7 +48,7 @@ public class Cerebro {
                                             if (s.hasNextLine())
                                             {
                                                 matrixI[i][j] =  s.nextInt(); 
-                                             System.out.println("  "+matrixI[i][j]);  
+                                             //System.out.println("  "+matrixI[i][j]);  
                                             }
 
                                     }
@@ -58,72 +56,89 @@ public class Cerebro {
 				
 			
 			s.close();
-                         for (int i = 0; i < 9; i++){
-                                        for (int j = 0; j < 9; j++){
-                                            System.out.println("  "+matrixI[i][j]);   
-                                    }
-                                }
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
                 }
-      
+    }
+    public void matrizsolución(){
+        File f = new File("Matrizsolucion.txt");
+
+		Scanner s;
+		try {	
+                    //cargamos el archivo con la clase Scan
+                    s  = new Scanner(f);
+                    //Impirmimos el contenido
+			
+                                for (int i = 0; i < 9; i++){
+                                        for (int j = 0; j < 9; j++){
+                                            if (s.hasNextLine())
+                                            {
+                                                matrixS[i][j] =  s.nextInt();  
+                                                System.out.println(matrixS[i][j]);
+                                            }
+
+                                    }
+                                }
+				
+			
+			s.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+                }
     }
     
-    public void saveG ()throws IOException{
+    public void loadG(){
+        File f = new File("Matrizpartida.txt");
 
-        // https://www.programcreek.com/2011/03/java-write-to-a-file-code-example/
-        
-        File fnew=new File("C:\\Users\\jorge\\Documents\\NetBeansProjects\\sudoku\\src\\sudoku\\partida.txt");
-        String source;
-        FileOutputStream fos = new FileOutputStream(fnew);
- 	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-        
-                    
-                    //c
-         try{
-       
+		Scanner s;
+		try {	
+                    //cargamos el archivo con la clase Scan
+                    s  = new Scanner(f);
+                    //Impirmimos el contenido
+			
+                                for (int i = 0; i < 9; i++){
+                                        for (int j = 0; j < 9; j++){
+                                            if (s.hasNextLine())
+                                            {
+                                                matrixP[i][j] =  s.nextLine();  
+                                            }
 
-        for (int i = 0; i < 9; i++){
+                                    }
+                                }
+				
+			
+			s.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+                }
+    }
+   public void saveG(){
+        try {
+            String ruta = "Matrizpartida.txt";
+            
+            File file = new File(ruta);
 
-            for (int j = 0; j < 9; j++){
-                
-                if ( matrizPP[i][j].equals("")){
-                source = "0";
-                } else{
-                source = matrizPP[i][j];;
-                
-                } 
-
-        bw.write(source);
-        bw.newLine();
-  
-          
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
             }
             
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (int i = 0; i < 9; i++){
+                                        for (int j = 0; j < 9; j++){
+                                         
+                                              contenido = matrixP [i][j];
+                                              bw.write(contenido);
+                                                bw.newLine();
+                                    }
+                                }
+            
+            
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
-        bw.close();
-
-         }catch (IOException e) {
-
-			e.printStackTrace();
-
-                }
-
-    }    
-
-    
-    public void loadG(){
-        
-      
-    }    
-    
-    public boolean checkWin(){
-        boolean winner;
-        winner=false;
-        return winner;
-    }    
-    
-    
-    
+    }
+   
 }
